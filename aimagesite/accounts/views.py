fc4d3f64824dff07d5a1a4372ab4b653f4dc5ecc
Tomasz Strzoda <strzoda.tomasz@gmail.com>
2020-11-22 18:06:12 +0100

    Add logging in

--- aimagesite/accounts/views.py
+++ aimagesite/accounts/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
-# Create your views here.
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # log the user in
+            user = form.save()
+            # log in the user
+            login(request, user)
             return redirect('homepage')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', { 'form': form })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log in the user
+            user = form.get_user()
+            login(request, user)
+            return redirect('homepage')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', { 'form': form })
