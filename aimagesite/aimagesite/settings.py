6195b56c0fa940b6d79f3977c1281fca1be58ab1
diejdablju <diejdablju@gmail.com>
2020-12-10 21:45:27 +0100

    Add media settings

--- aimagesite/aimagesite/settings.py
+++ aimagesite/aimagesite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
-    "images.apps.ImagesConfig",
+    "images",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
