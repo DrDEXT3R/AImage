a1df1896e388384d5fd00110684b1425da1533d1
diejdablju <diejdablju@gmail.com>
2020-12-10 21:42:11 +0100

    Add ImageField; override __str__ metod to return name of the image; add metod that specifies what url to go after saving image

--- aimagesite/images/models.py
+++ aimagesite/images/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
 
 class Image(models.Model):
-    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default=timezone.now)
-    # header_image = models.ImageField(upload_to="ImagesDB/")
+    header_image = models.ImageField(null=True, upload_to="ImagesDB/")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("images:image-list", kwargs={"id": self.author_id})
