6c6ef5d6e3451a3f9fac71cc4e5ac10acad9aaa3
diejdablju <diejdablju@gmail.com>
2020-12-10 21:25:47 +0100

    Change function-based upload view into class-based upload view; Add class-based user's images list view

--- aimagesite/images/urls.py
+++ aimagesite/images/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from .views import ImageUploadView, ImageListView
 from . import views
 
+app_name = "images"
+
 urlpatterns = [
-    path("upload/", views.upload, name="upload"),
+    path("upload/", ImageUploadView.as_view(), name="upload"),
+    path("list/<int:id>/", ImageListView.as_view(), name="image-list"),
+    # path("upload/", views.upload, name="upload"),
 ]
--- aimagesite/images/views.py
+++ aimagesite/images/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.views.generic import CreateView, ListView
 from .forms import UploadForm
+from .models import Image
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -8,8 +11,31 @@ def upload(request):
 
     if request.method == "POST":
         form = UploadForm(request.POST)
+        form.instance.author = request.user
         form.save()
         return redirect("homepage")
     else:
         form = UploadForm()
     return render(request, "images/upload.html", {"form": form})
+
+
+class ImageUploadView(LoginRequiredMixin, CreateView):
+
+    model = Image
+    template_name = "images/upload.html"
+    fields = ["name", "header_image"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class ImageListView(LoginRequiredMixin, ListView):
+
+    model = Image
+    template_name = "images/user_images_list.html"
+    context_object_name = "images"
+    ordering = ["-date_posted"]
+
+    def get_queryset(self):
+        return Image.objects.filter(author=self.kwargs["id"])
