2f2568abbd2bc2bb595f00220d6eccd7fa91ae8f
diejdablju <diejdablju@gmail.com>
2020-12-10 21:46:13 +0100

    Add static with media settings

--- aimagesite/aimagesite/urls.py
+++ aimagesite/aimagesite/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("about/", views.about),
     path("", views.homepage, name="homepage"),
     path("accounts/", include("accounts.urls")),
     path("images/", include("images.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
