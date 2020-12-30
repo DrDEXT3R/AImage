f362269c0aba95c0ad46eb0ba78df1b2b0e30ae9
diejdablju <diejdablju@gmail.com>
2020-12-30 09:18:43 +0100

    Add functionality of deleting the image from the server

--- aimagesite/images/urls.py
+++ aimagesite/images/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ImageUploadView, ImageListView
+from .views import ImageUploadView, ImageListView, ImageDeleteView
 from . import views
 
 app_name = "images"
@@ -7,5 +7,6 @@ app_name = "images"
 urlpatterns = [
     path("upload/", ImageUploadView.as_view(), name="upload"),
     path("list/<int:id>/", ImageListView.as_view(), name="image-list"),
+    path("delete/<int:pk>/", ImageDeleteView.as_view(), name="image-delete"),
     # path("upload/", views.upload, name="upload"),
 ]
--- aimagesite/images/views.py
+++ aimagesite/images/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView, ListView
+from django.views.generic.edit import FormView
+from django.views.generic import CreateView, ListView, DeleteView
 from .forms import UploadForm
 from .models import Image
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from PIL import Image as PILimage
+from urllib.request import urlopen
+from io import BytesIO
+from django.core.files.base import ContentFile
+from django.urls import reverse
+from urllib.parse import urlsplit, urlparse
+import os.path
+import http.client
+
+import sys
 
 # Create your views here.
 
-
+"""
 def upload(request):
 
     if request.method == "POST":
         form = UploadForm(request.POST)
         form.instance.author = request.user
+        # save img from url TODO
         form.save()
-        return redirect("homepage")
+        return redirect("http://localhost:8000/images/list/" + str(request.user.id))
     else:
         form = UploadForm()
     return render(request, "images/upload.html", {"form": form})
+"""
 
 
 class ImageUploadView(LoginRequiredMixin, CreateView):
@@ -35,7 +48,24 @@ class ImageListView(LoginRequiredMixin, ListView):
     model = Image
     template_name = "images/user_images_list.html"
     context_object_name = "images"
-    ordering = ["-date_posted"]
 
     def get_queryset(self):
-        return Image.objects.filter(author=self.kwargs["id"])
+        return (
+            Image.objects.filter(author=self.kwargs["id"])
+            .order_by("date_posted")
+            .reverse()
+        )
+
+
+class ImageDeleteView(LoginRequiredMixin, DeleteView):
+    model = Image
+
+    def test_func(self):
+        image = self.get_object()
+        if self.request.user == image.author:
+            return True
+        return False
+
+    def get_success_url(self):
+        image = self.get_object()
+        return reverse("images:image-list", kwargs={"id": image.author_id})
